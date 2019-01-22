084c7bf15368f83253641ab270e0ea3c12ae2aea
daryl <15671651407@163.com>
2019-01-22 09:07:19 +0800

    付费视频

--- js/global.js
+++ js/global.js
@@ -1,8 +1,11 @@
 const protocol="http://"
-const domain="192.168.1.177";
-const port = "9090";
+// const domain="192.168.1.177";
+const domain="39.98.204.123";
+// const port = "9090";
+const port = "8080";
 const project = "/RedCross/";
 
 function urlFactory(url){
-	return protocol + domain + ":" + port + project + url;
+	return protocol + domain + ":" + port + project + url + 
+	"?token=" + "eyJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lcklkIjo1LCJleHBpcmVzIjoxNTQ4MDk0OTA0MzU3fQ.iyRGY38-jVBqCvXrxczM8s9B8i0xYUCzDvax9Cn16v4";
 }
