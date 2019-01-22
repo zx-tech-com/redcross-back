2b8ec7addb85851eca68b907ce6b2d7b9093d91e
daryl <15671651407@163.com>
2019-01-22 17:26:41 +0800

    付费视频管理

--- js/global.js
+++ js/global.js
@@ -1,8 +1,8 @@
 const protocol="http://"
-// const domain="192.168.1.177";
-const domain="39.98.204.123";
-// const port = "9090";
-const port = "8080";
+const domain="192.168.1.177";
+// const domain="39.98.204.123";
+const port = "9090";
+// const port = "8080";
 const project = "/RedCross/";
 
 
@@ -12,12 +12,12 @@ const SUCCESS = "success";
 function urlFactory(url){
 	if(!url){//url为空
 		return protocol + domain + ":" + port + project.substring(0,project.length-1) +
-		"?token=" + "eyJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lcklkIjo1LCJleHBpcmVzIjoxNTQ4MTQ4MjI3Njc1fQ.P18OMnw3D0Gplab1O5UurrMx3QJ1Z0gPf3_k4JPnoZk";
+		"?token=" + "eyJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lcklkIjo1LCJleHBpcmVzIjoxNTQ4MTc3MjQzNTM2fQ.KQudA_KWnSvYX2oZK9L3ftNwi02rBNT8p3U6Uu0IT7A";
 
 	}else{
 		var s = url.indexOf('?') == -1 ? '?' : '&';
 		return protocol + domain + ":" + port + project + url + s +
-		"token=" + "eyJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lcklkIjo1LCJleHBpcmVzIjoxNTQ4MTQ4MjI3Njc1fQ.P18OMnw3D0Gplab1O5UurrMx3QJ1Z0gPf3_k4JPnoZk";
+		"token=" + "eyJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lcklkIjo1LCJleHBpcmVzIjoxNTQ4MTc3MjQzNTM2fQ.KQudA_KWnSvYX2oZK9L3ftNwi02rBNT8p3U6Uu0IT7A";
 	}
 	
 }
