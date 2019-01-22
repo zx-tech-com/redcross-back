3422a70035727b04996a79d824e4000f928292da
root <1137013957@qq.com>
2019-01-22 09:09:47 +0800

    后台1/22 9:09

--- js/global.js
+++ js/global.js
@@ -1,5 +1,7 @@
 const protocol="http://"
-const domain="192.168.1.177";
+//const domain="192.168.1.177";
+//const domain="39.98.204.123";
+const domain="192.168.1.100";
 const port = "9090";
 const project = "/RedCross/";
 
