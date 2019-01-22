2980bfcd1c843a70f1b88493def2a9904a534180
daryl <15671651407@163.com>
2019-01-22 10:19:28 +0800

    修改global.js

--- js/global.js
+++ js/global.js
@@ -5,7 +5,19 @@ const domain="39.98.204.123";
 const port = "8080";
 const project = "/RedCross/";
 
+
+const SUCCESS = "success";
+
+
 function urlFactory(url){
-	return protocol + domain + ":" + port + project + url + 
-	"?token=" + "eyJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lcklkIjo1LCJleHBpcmVzIjoxNTQ4MDk0OTA0MzU3fQ.iyRGY38-jVBqCvXrxczM8s9B8i0xYUCzDvax9Cn16v4";
+	if(!url){//url为空
+		return protocol + domain + ":" + port + project.substring(0,project.length-1) +
+		"?token=" + "eyJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lcklkIjo1LCJleHBpcmVzIjoxNTQ4MTQ4MjI3Njc1fQ.P18OMnw3D0Gplab1O5UurrMx3QJ1Z0gPf3_k4JPnoZk";
+
+	}else{
+		var s = url.indexOf('?') == -1 ? '?' : '&';
+		return protocol + domain + ":" + port + project + url + s +
+		"token=" + "eyJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lcklkIjo1LCJleHBpcmVzIjoxNTQ4MTQ4MjI3Njc1fQ.P18OMnw3D0Gplab1O5UurrMx3QJ1Z0gPf3_k4JPnoZk";
+	}
+	
 }
